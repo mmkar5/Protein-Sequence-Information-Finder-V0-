fe190e0c98aa9c0927ee0bdbcb3914c209ea0874
Madhabendra Mohon Kar <160476301+mmkar5@users.noreply.github.com>
2024-05-04 12:58:46 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,107 @@
+ Protein Sequence Property Information Finder (V0)
+#### Video Demo: <https://youtu.be/OXrPTR6PThM->
+#### Description:
+The program project.py takes an id & protein sequence or an input fasta file containing several sequences & provides information regarding the following features of the given protein sequence:
+- Length of the sequence
+- Number of each amino acids
+- Percentage of each amino acids
+- Percentage of amino acids in each groups: nonpolar, aromatic, polar, all_charged, positive_charged, negative_charged
+- Protein molecular weight
+- Protein sequence motif presence and location
+- GRAVY (grand average of hydropathy) score
+
+This program considers the standard 20 common amino acids and does not take into acoount any modified amino acids.
+
+This project also contains test_project.py designed to test all the functions implemented in project.py
+
+`usage: python project.py -h -i I -o O -m -w`
+
+The program takes the following command line arguments to determine properties of a protein sequence:
+```
+-h, --help  show this help message and exit
+
+-i I        Enter input filepath of a protein fasta file (optional)
+
+-o O        Enter output filepath (optional)
+
+-m          Search for motifs (optional)
+
+-w          get monoisoptopic molecular weight (optional)
+```
+If no input filepath for a fasta file is provides, will propmpt user to enter "Enter protein sequence identifier: " and subsequently in next line, "Enter protein sequence: ". If the input filename is not a ".fasta" file or does not exist, the program will exit, indicating that "File not found" or "Not a fasta file" respectively.
+
+If output filepath is provided, then the outputs will be saved in the provided file, otherwise it will will be printed on the command line interface. If the output file already exists, then the program will exit, indicating "Given output file already exists!", preventing any overwriting on an existing file.
+
+if -w is provided as a command line argument, then the monoisotopic molecular weight will be calculated for the protein sequence. Otherwise, by default, the averal molecular weight will be calculated.
+
+if -m is provided as a command line argument, then user will be prompted, to provide a regular expression pattern to be matched as a protein sequence motif for each of the given protein sequence. The format of regular expression is printed to the user with an example to make it easier for the user to enter the correct input.
+
+Apart from the main function, there are the following functions defined at the same indentation level as main:
+-   get_args():
+        Obtains command-line arguments from the user and parses them.
+-   get_input_seq(args):
+        Obtains input from the user (a fasta file or a sequnce id and sequence in the command-line) and
+    returns a generator of the sequence id(s) and sequence(s).
+-   get_motif():
+        Obtains a motif from the user as a regular expression and
+    returns the motif
+
+All the methods used for obtaining the protein sequence features are implemented within a class called Protein_Seq_Features. The object of the class is initialised as, `protein = Protein_Seq_Features(seq)`,where seq is an instance attribute, indicaing the protein sequence. The value of seq is checked to ensure it is made up of alphbets, otherwise there will be a ValueError indicating "No valid sequence given". The methods of this class are:
+-   seq_length(self):
+        Returns the length of the protein sequence
+-   aa_composition_num(self):
+        Returns a dictionary containing the number of each of 20 amino acids present in the protein sequence
+-   aa_composition_percentage(self):
+        Returns a dictionary containing relative percentage of each of 20 amino acids present in the protein sequence
+-   aa_group_percentage(self):
+        Returns a dictionary containing percentage of nonpolar,aromatic,polar,all_charged, positive_charged
+        and negative_charged amino acids present in the protein sequence
+-   Mw(self, use_monoisotopic_mass=False):
+        Returns the  average or monoisotopic molecular weight of the protein, depending on the argument of use_monotopic_mass;
+        by default, returns the average molecular weight
+-   motif_finder(self, motif):
+        Takes a arguemt of the motif as a regular experssion, and returns the motifs matched and their position,
+        if there is any match.
+-   gravy_score(self):
+        Returns the GRAVY (grand average of hydropathy) values of a protein sequence,
+        which is a measure of the hydrophobicity or hydrophilicity of the protein sequence.
+
+Example-
+
+input.fasta:
+>sp|P02748|CO9_HUMAN Complement component C9 OS=Homo sapiens OX=9606 GN=C9 PE=1 SV=2
+MSACRSFAVAICILEISILTAQYTTSYDPELTESSGSASHIDCRMSPWSEWSQCDPCLRQ
+MFRSRSIEVFGQFNGKRCTDAVGDRRQCVPTEPCEDAEDDCGNDFQCSTGRCIKMRLRCN
+GDNDCGDFSDEDDCESEPRPPCRDRVVEESELARTAGYGINILGMDPLSTPFDNEFYNGL
+CNRDRDGNTLTYYRRPWNVASLIYETKGEKNFRTEHYEEQIEAFKSIIQEKTSNFNAAIS
+LKFTPTETNKAEQCCEETASSISLHGKGSFRFSYSKNETYQLFLSYSSKKEKMFLHVKGE
+IHLGRFVMRNRDVVLTTTFVDDIKALPTTYEKGEYFAFLETYGTHYSSSGSLGGLYELIY
+VLDKASMKRKGVELKDIKRCLGYHLDVSLAFSEISVGAEFNKDDCVKRGEGRAVNITSEN
+LIDDVVSLIRGGTRKYAFELKEKLLRGTVIDVTDFVNWASSINDAPVLISQKLSPIYNLV
+PVKMKNAHLKKQNLERAIEDYINEFSVRKCHTCQNGGTVILMDGKCLCACPFKFEGIACE
+ISKQKISEGLPALEFPNEK
+
+Terminal:
+```
+python.py -i input.fasta -o output.txt -m
+{n} matches number of repeating units
+{m,n} matches m to n repeating units
+. matches any amino acid a|b indicates a or b
+[] matches sets of charectars
+[^] indicates charectars not in set
+^ at beginning matches from N terminal and $ at end matches at C terminal
+Example of C2H2 Zinc Finger motif: C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H
+Enter pattern of motif in regular expression: N[^P][ST][^P]
+```
+output.txt:
+```
+ID:sp|P02748|CO9_HUMAN Complement component C9 OS=Homo sapiens OX=9606 GN=C9 PE=1 SV=2
+MSACRSFAVAICILEISILTAQYTTSYDPELTESSGSASHIDCRMSPWSEWSQCDPCLRQMFRSRSIEVFGQFNGKRCTDAVGDRRQCVPTEPCEDAEDDCGNDFQCSTGRCIKMRLRCNGDNDCGDFSDEDDCESEPRPPCRDRVVEESELARTAGYGINILGMDPLSTPFDNEFYNGLCNRDRDGNTLTYYRRPWNVASLIYETKGEKNFRTEHYEEQIEAFKSIIQEKTSNFNAAISLKFTPTETNKAEQCCEETASSISLHGKGSFRFSYSKNETYQLFLSYSSKKEKMFLHVKGEIHLGRFVMRNRDVVLTTTFVDDIKALPTTYEKGEYFAFLETYGTHYSSSGSLGGLYELIYVLDKASMKRKGVELKDIKRCLGYHLDVSLAFSEISVGAEFNKDDCVKRGEGRAVNITSENLIDDVVSLIRGGTRKYAFELKEKLLRGTVIDVTDFVNWASSINDAPVLISQKLSPIYNLVPVKMKNAHLKKQNLERAIEDYINEFSVRKCHTCQNGGTVILMDGKCLCACPFKFEGIACEISKQKISEGLPALEFPNEK
+Length:559
+Number of amino acids:{'A': 29, 'V': 28, 'I': 33, 'L': 42, 'M': 10, 'P': 19, 'G': 36, 'C': 26, 'F': 28, 'Y': 21, 'W': 4, 'S': 48, 'T': 32, 'N': 27, 'Q': 14, 'H': 9, 'K': 37, 'R': 33, 'D': 35, 'E': 48}
+Percentage of amino acids:{'A': 5.19, 'V': 5.01, 'I': 5.9, 'L': 7.51, 'M': 1.79, 'P': 3.4, 'G': 6.44, 'C': 4.65, 'F': 5.01, 'Y': 3.76, 'W': 0.72, 'S': 8.59, 'T': 5.72, 'N': 4.83, 'Q': 2.5, 'H': 1.61, 'K': 6.62, 'R': 5.9, 'D': 6.26, 'E': 8.59}
+Percentage of amino acids in respective groups:{'nonpolar': 39.89, 'aromatic': 9.48, 'polar': 21.65, 'all_charged': 28.98, 'positive_charged': 8.23, 'negative_charged': 20.75}
+Molecular Weight:63173.376 Da
+GRAVY Score:-0.45
+Motifs matched:[{'motif_match': 'NETY', 'position': (277, 280)}, {'motif_match': 'NITS', 'position': (415, 418)}]
+```
---
+++ project.py
@@ -0,0 +1,329 @@
+import argparse
+import sys
+import re
+import os
+
+
+class Protein_Seq_Features:
+    amino_acids = [
+        "A",
+        "V",
+        "I",
+        "L",
+        "M",
+        "P",
+        "G",
+        "C",
+        "F",
+        "Y",
+        "W",
+        "S",
+        "T",
+        "N",
+        "Q",
+        "H",
+        "K",
+        "R",
+        "D",
+        "E",
+    ]
+
+    def __init__(self, seq):
+        self.seq = seq
+
+    def seq_length(self):
+        """Returns the length of the protein sequence"""
+
+        return len(self.seq)
+
+    def aa_composition_num(self):
+        """Returns a dictionary containing the number of each of 20 amino acids present in the protein sequence"""
+
+        return {aa: self.seq.count(aa) for aa in Protein_Seq_Features.amino_acids}
+
+    def aa_composition_percentage(self):
+        """Returns a dictionary containing relative percentage of each of 20 amino acids present in the protein sequence"""
+
+        return {
+            aa: round(self.seq.count(aa) / len(self.seq) * 100, 2)
+            for aa in Protein_Seq_Features.amino_acids
+        }
+
+    def aa_group_percentage(self):
+        """Returns a dictionary containing percentage of nonpolar,aromatic,polar,all_charged, positive_charged
+        and negative_charged amino acids present in the protein sequence """
+
+        self.nonpolar = round(
+            sum([self.seq.count(aa) for aa in ["A", "V", "I", "L", "M", "P", "G", "C"]])
+            / len(self.seq)
+            * 100,
+            2,
+        )
+        self.aromatic = round(
+            sum([self.seq.count(aa) for aa in ["F", "Y", "W"]]) / len(self.seq) * 100, 2
+        )
+        self.polar = round(
+            sum([self.seq.count(aa) for aa in ["S", "T", "N", "Q"]])
+            / len(self.seq)
+            * 100,
+            2,
+        )
+        self.all_charged = round(
+            sum([self.seq.count(aa) for aa in ["H", "K", "R", "D", "E"]])
+            / len(self.seq)
+            * 100,
+            2,
+        )
+        self.positive_charged = round(
+            sum([self.seq.count(aa) for aa in ["H", "K"]]) / len(self.seq) * 100, 2
+        )
+        self.negative_charged = round(
+            sum([self.seq.count(aa) for aa in ["R", "D", "E"]]) / len(self.seq) * 100, 2
+        )
+        return {
+            "nonpolar": self.nonpolar,
+            "aromatic": self.aromatic,
+            "polar": self.polar,
+            "all_charged": self.all_charged,
+            "positive_charged": self.positive_charged,
+            "negative_charged": self.negative_charged,
+        }
+
+    def Mw(self, use_monoisotopic_mass=False):
+        """Returns the  average or monoisotopic molecular weight of the protein, depending on the argument of use_monotopic_mass;
+        by default, returns the average molecular weight"""
+
+        monoisotopic_mass = {
+            "A": 71.03711,
+            "R": 156.10111,
+            "N": 114.04293,
+            "D": 115.02694,
+            "C": 103.00919,
+            "E": 129.04259,
+            "Q": 128.05858,
+            "G": 57.02146,
+            "H": 137.05891,
+            "I": 113.08406,
+            "L": 113.08406,
+            "K": 128.09496,
+            "M": 131.04049,
+            "F": 147.06841,
+            "P": 97.05276,
+            "S": 87.03203,
+            "T": 101.04768,
+            "W": 186.07931,
+            "Y": 163.06333,
+            "V": 99.06841,
+        }
+        average_mass = {
+            "A": 71.0788,
+            "R": 156.1875,
+            "N": 114.1038,
+            "D": 115.0886,
+            "C": 103.1388,
+            "E": 129.1155,
+            "Q": 128.1307,
+            "G": 57.0519,
+            "H": 137.1411,
+            "I": 113.1594,
+            "L": 113.1594,
+            "K": 128.1741,
+            "M": 131.1926,
+            "F": 147.1766,
+            "P": 97.1167,
+            "S": 87.0782,
+            "T": 101.1051,
+            "W": 186.2132,
+            "Y": 163.1760,
+            "V": 99.1326,
+        }
+        Mw = 0
+        if use_monoisotopic_mass:
+            return round(
+                sum(
+                    [
+                        self.seq.count(aa) * monoisotopic_mass[aa]
+                        for aa in monoisotopic_mass
+                    ]
+                )
+                + 18.010565,
+                3,
+            )
+        else:
+            return round(
+                sum([self.seq.count(aa) * average_mass[aa] for aa in average_mass])
+                + 18.01528,
+                3,
+            )
+
+    def motif_finder(self, motif):
+        """Takes a arguemt of the motif as a regular experssion, and returns the motifs matched and their position,
+        if there is any match."""
+
+        if motif:
+            matched = [
+                {"motif_match": match.group(), "position": (match.start()+1,match.end())}
+                for match in re.finditer(motif.strip(), self.seq, re.I)
+            ]
+            if matched:
+                return matched
+            else:
+                return f"No such motif exits in sequence"
+        else:
+            return f"The input is empty. Please provide a valid motif."
+
+    def gravy_score(self):
+        """Returns the GRAVY (grand average of hydropathy) values of a protein sequence,
+        which is a measure of the hydrophobicity or hydrophilicity of the protein sequence."""
+
+        hydropathy_values = {
+
+            "A": 1.800,
+            "R": -4.500,
+            "N": -3.500,
+            "D": -3.500,
+            "C": 2.500,
+            "E": -3.500,
+            "Q": -3.500,
+            "G": -0.400,
+            "H": -3.200,
+            "I": 4.500,
+            "L": 3.800,
+            "K": -3.900,
+            "M": 1.900,
+            "F": 2.800,
+            "P": -1.600,
+            "S": -0.800,
+            "T": -0.700,
+            "W": -0.900,
+            "Y": -1.300,
+            "V": 4.200,
+        }
+
+        return round(
+            sum(
+                [self.seq.count(aa) * hydropathy_values[aa] for aa in hydropathy_values]
+            )
+            / len(self.seq),
+            3,
+        )
+
+    @property
+    def seq(self):
+        return self.__seq
+
+    @seq.setter
+    def seq(self, seq):
+        if not seq.isalpha():
+            raise ValueError("No sequence given")
+        self.__seq = seq
+
+
+def main():
+
+    args = get_args()
+    if args.m:
+        motif = get_motif()
+
+    if args.o:
+        if os.path.exists(args.o):
+            sys.exit("Given output file already exists!")
+        else:
+            output_file = open(args.o, "w")
+    else:
+        output_file = None
+
+    for seq_id, seq in get_input_seq(args.i):
+        protein = Protein_Seq_Features(seq)
+        results = [
+            f"ID:{seq_id}",
+            seq,
+            f"Length:{protein.seq_length()}",
+            f"Number of amino acids:{protein.aa_composition_num()}",
+            f"Percentage of amino acids:{protein.aa_composition_percentage()}",
+            f"Percentage of amino acids in respective groups:{protein.aa_group_percentage()}",
+            f"Molecular Weight:{protein.Mw(args.w)} Da",
+            f"GRAVY Score:{protein.gravy_score()}",
+        ]
+        if args.m:
+            results.append(f"Motifs matched:{protein.motif_finder(motif)}")
+
+        for result in results:
+            if output_file:
+                output_file.write(result + "\n")
+            else:
+                print(result)
+
+    if output_file:
+        output_file.close()
+
+
+def get_args():
+    """Obtains command-line arguments from the user and parses them"""
+    parser = argparse.ArgumentParser(
+        description="Arguments to determine properties of a protein sequence"
+    )
+    parser.add_argument(
+        "-i",
+        required=False,
+        type=str,
+        help="Enter input filepath of a protein fasta file",
+    )
+    parser.add_argument("-o", required=False, type=str, help="Enter output filepath")
+    parser.add_argument("-m", action="store_true", help="Search for motifs")
+    parser.add_argument(
+        "-w", action="store_true", help="get monoisoptopic molecular weight"
+    )
+    args = parser.parse_args()
+    return args
+
+
+def get_input_seq(args):
+    """Obtains input from the user (a fasta file or a sequnce id and sequence in the command-line) and
+    returns a generator of the sequence id(s) and sequence(s)."""
+
+    if args:
+        try:
+            if ".fasta" in args:
+                with open(args, "r") as file:
+                    seq=""
+                    seq_id=""
+                    for line in file:
+                        line=line.strip()
+                        if line.startswith(">"):
+                            if seq:
+                                yield seq_id,seq
+                                seq=""
+                            seq_id=line.lstrip(">")
+                        else:
+                            seq+=line
+                    yield seq_id,seq
+            else:
+                raise ValueError
+        except FileNotFoundError:
+            sys.exit("File not found")
+        except ValueError:
+            sys.exit("Not a fasta file")
+    else:
+        seq_id = input("Enter protein sequence identifier: ")
+        seq = input("Enter protein sequence: ").strip().upper()
+        yield seq_id, seq
+
+
+def get_motif():
+    """Obtains a motif from the user as a regular expression and
+    returns the motif"""
+
+    print("{n} matches number of repeating units")
+    print("{m,n} matches m to n repeating units")
+    print(". matches any amino acid", "a|b indicates a or b")
+    print("[] matches sets of charectars")
+    print("[^] indicates charectars not in set")
+    print("^ at beginning matches from N terminal and $ at end matches at C terminal")
+    print("Example of C2H2 Zinc Finger motif: C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H")
+    motif = input("Enter pattern of motif in regular expression: ").strip()
+    return motif
+
+
+if __name__ == "__main__":
+    main()
